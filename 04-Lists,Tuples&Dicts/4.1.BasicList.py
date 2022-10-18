5903a204b4996a0b5de77d453621ed8b77ce1c72
Ashaba <ashabajasper@gmail.com>
2022-10-18 22:19:37 +0300

    This is an updated change

--- 04-Lists,Tuples&Dicts/4.1.BasicList.py
+++ 04-Lists,Tuples&Dicts/4.1.BasicList.py
@@ -1,2 +1,11 @@
+#is a value that contains multiple values in an ordered sequence
+list1=[1,2,3]
+list2=['cat','bat','rat','elephant']
+list3=[12,45,65]
+print(list1)
+print(list2)
+print(list3)
 
-listname=[1,2,3]
+#access elements in a list by using indexes
+print(list1[0])
+print(list1[-1])

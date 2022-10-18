af955bdd597f04b3666d93aa3e3c7eb3162d97d4
Ashaba <ashabajasper@gmail.com>
2022-10-18 13:04:21 +0300

    Update-5

---
+++ 01-Basics/Input()str()&Concatenation.py
@@ -0,0 +1,77 @@
+#input() function waits for the user to type text on the keyboard and press enter
+#variable_name=input()
+#print(variable_name) will output what the person typed in the program
+
+name1=input()
+print(name)
+
+#we usually want to prompt the user with a message hence useful that you ask what they should input
+name2=input("Please enter your name:")
+print(name2)
+
+#the input() accepts only strings, hence to input and print numbers, concatenation is applied
+#CONCATENATION
+first_name="Bob"
+last_name="Smith"
+
+print(first_name+last_name) # output BobSmith
+print(first_name+" "+last_name) #output Bob Smith
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

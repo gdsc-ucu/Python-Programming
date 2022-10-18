258706890c97f2d73050032d0bc7d36730dedb7e
Ashaba <ashabajasper@gmail.com>
2022-10-18 21:15:07 +0300

    This is an updated change

--- 03-Functions/2.1.BasicFunction.py
+++ 03-Functions/2.1.BasicFunction.py
@@ -5,9 +5,4 @@ def hello():
     print("Your trainer for this session")
 
 hello()
-=======
-def hello():
-    print("Hello There")
-    print("My name is Jasper")
-    print("Your trainer for this session")
->>>>>>> origin/main
+

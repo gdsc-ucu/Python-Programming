093739ff75f42888daa089ee301382216636490e
Ashaba <ashabajasper@gmail.com>
2022-10-18 11:47:47 +0300

    Update-1

--- 01-Basics/Expressions&Print.py
+++ 01-Basics/Expressions&Print.py
@@ -1,1 +1,5 @@
-#Introduction to the print statement
+#Introduction to the print statement[syntax-print()]
+#performs any arithmetic operations(+,-,*,/) with numbers
+#[syntax-print((numberone)(+,-,*,/)(numbertwo))
+
+print(2+2) #will output 4

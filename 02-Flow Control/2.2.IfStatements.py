6b45f5d8b84cb7876442b2fc6da15e29f2b38f7d
Ashaba <ashabajasper@gmail.com>
2022-10-18 15:37:37 +0300

    This is an updated change

--- 02-Flow Control/2.2.IfStatements.py
+++ 02-Flow Control/2.2.IfStatements.py
@@ -37,3 +37,21 @@ count=0
 if count<5:
     print("Hello World")
     count=count+1
+
+#if statment to check max of 3 numbers
+#write a program that allows you to find the largest of three or more numbers
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+num3=int(input("Enter the third number:"))
+#comparing the first number
+if num1>num2 and num1>num3:
+     print(num1, "is greater than ",num2,"and",num3, "\n")
+#checking for equality
+elif num1==num2==num3:
+        print(num1, "is equal to ",num2,"and",num3, "\n")
+#comparing the second number
+elif num2>num1 and num2>num3:
+     print(num2, "is greater than ",num1,"and",num3, "\n")
+#comparing the third number
+else:
+    print(num3, "is greater than ",num1,"and",num2, "\n")

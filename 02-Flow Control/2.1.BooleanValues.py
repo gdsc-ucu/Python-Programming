10fb1627acb869c3eceeb1b28b3a53c9761e3aed
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2022-10-18 14:04:10 +0300

    Add files via upload

---
+++ 02-Flow Control/2.1.BooleanValues.py
@@ -0,0 +1,9 @@
+#FLow control decide which Python instructions execute under which conditions
+#How to represent yes and no options
+#BOOLEAN DATA TYPE
+#Has two values True and False
+name=True
+print(name)
+#True or False cannot be used as variable names
+
+#Booleans work with Comparison Operators

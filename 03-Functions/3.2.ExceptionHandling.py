1ad5f3b2a3cb0205dbaa1a25948db06b1c29f915
Ashaba <ashabajasper@gmail.com>
2022-10-18 21:53:19 +0300

    This is an updated change

--- 03-Functions/3.2.ExceptionHandling.py
+++ 03-Functions/3.2.ExceptionHandling.py
@@ -6,4 +6,44 @@ def division(divisor):
 print(division(2))
 print(division(12))
 print(division(-29))
-print(divison(0))
+#when divided by zero an error will appear
+#print(division(0))
+
+#using exception handling
+def division(divisor):
+    try:
+        return 2022/divisor
+    except ZeroDivisionError:
+        print("ERROR, cannot divide by zero")
+
+print(division(2))
+print(division(12))
+print(division(-29))
+print(division(0))
+
+#Project
+import time, sys
+indent = 0 # How many spaces to indent.
+indentIncreasing = True # Whether the indentation is increasing or not.
+
+try:
+    while True: # The main program loop.
+        print(' ' * indent, end='')
+        print('********')
+        time.sleep(0.1) # Pause for 1/10 of a second.
+
+        if indentIncreasing:
+            # Increase the number of spaces:
+            indent = indent + 1
+            if indent == 20:
+                # Change direction:
+                indentIncreasing = False
+
+        else:
+            # Decrease the number of spaces:
+            indent = indent - 1
+            if indent == 0:
+                # Change direction:
+                indentIncreasing = True
+except KeyboardInterrupt:
+    sys.exit()

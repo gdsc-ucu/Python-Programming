3046f04792c11c87dc9bed03a84d98f0610ec07f
Ashaba <ashabajasper@gmail.com>
2022-10-18 13:41:28 +0300

    Update-5

--- 01-Basics/FirstProgram.py
+++ 01-Basics/FirstProgram.py
@@ -13,4 +13,11 @@ print("Very nice to meet you "+first_name+" "+second_name)
 #alternatively
 #print(f"Very nice to meet you {first_name} {second_name}")
 
-print("As a a funfact, the length of your name is "+len(first_name+second_name))
+#len() is an int hence concatenation is needed
+print("As a a funfact, the length of your name is "+str(len(first_name+second_name)))
+
+print("Before you go, How old are you?")
+age=int(input())
+print("Wow"+first_name+" You are "+str(age)+"years old!")
+#how old they will be in 10 years
+print("Oh and by the way, you will be "+str(age+10)+" in 10 years")

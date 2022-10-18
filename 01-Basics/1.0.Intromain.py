8009cc2c129ea2a5ddf7e662bbfde6ee738a442f
Ashaba <ashabajasper@gmail.com>
2022-10-18 12:47:58 +0300

    Update-4

--- 01-Basics/1.0.Intromain.py
+++ 01-Basics/1.0.Intromain.py
@@ -1,3 +1,8 @@
+#USING PYCHARM EDITOR FOR PYTHON
+
+#Remeber a # is how we comment
+#There aren't any multi-line comments but you can use docstrings below
+"""I am here to remind you this a multiline comment"""
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.

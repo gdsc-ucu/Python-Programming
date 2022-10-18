55306af20eba1e1457b8ce4a72eead91f9497551
Ashaba <ashabajasper@gmail.com>
2022-10-18 14:13:48 +0300

    This is an updated change

--- 02-Flow Control/2.1.BooleanValues.py
+++ 02-Flow Control/2.1.BooleanValues.py
@@ -16,3 +16,11 @@ print(name)
 #   <=             Less than or equal to
 #   >=             Greater than or equal to
 #Test
+print(2==2)
+print(2!=4)
+print(17<90)
+print(100>3)
+print(1<=2)
+print(45>=23)
+#all return true, now try to reverse the numbers and observe what you get
+

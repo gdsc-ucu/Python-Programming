e15b9a45cfb143c2758de8b1048290d6868a5871
Ashaba <ashabajasper@gmail.com>
2022-10-18 21:23:30 +0300

    This is an updated change

--- 03-Functions/2.1.BasicFunction.py
+++ 03-Functions/2.1.BasicFunction.py
@@ -8,3 +8,10 @@ def hello():
 #calling the function
 hello()
 
+#function with parameters
+def hello(name):
+    print(f"Hello {name}, how are you doing today.")
+    print(f"{name} is such a nice name")
+
+hello("James") #will print the body of the text with the name filled with the provided name
+hello("Moses")

65ceea64fb290cb9db72c09ca86a1e866322b083
Ashaba <ashabajasper@gmail.com>
2022-10-18 13:33:57 +0300

    Update-5

---
+++ 01-Basics/FirstProgram.py
@@ -0,0 +1,14 @@
+#This program will ask for all your names, age, height and display them
+print("Enter your first name:")
+first_name=str(input())
+#alternatively
+#first_name=str(input("Please enter your first name:"))
+
+print("Enter your second name:")
+second_name=str(input())
+#alternatively
+#second_name=str(input("Please enter your second name:")
+
+print("Very nice to meet you "+first_name+" "+second_name)
+#alternatively
+#print(f"Very nice to meet you {first_name} {second_name}")

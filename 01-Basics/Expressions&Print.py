daf2700213a1ea8e7c48c4a5811b25c646679424
Ashaba <ashabajasper@gmail.com>
2022-10-18 11:55:35 +0300

    Update-3

--- 01-Basics/Expressions&Print.py
+++ 01-Basics/Expressions&Print.py
@@ -1,9 +1,14 @@
 #Introduction to the print statement[syntax-print("statement")]
 print("Hello World") #will output the phrase Hello World
+
 print("2") #will output the number 2 but as a string
 print(2) #will output the number 2 as an interger
-#performs any arithmetic operations(+,-,*,/) with numbers
+
+#print can also perform basic arithmetic operations(+,-,*,/) with numbers
 #[syntax-print((numberone)(+,-,*,/)(numbertwo))
 
-print(2+2) #will output 4
+print(2+2) # =4
+print(10/2) # =5
+print(4*4) # =16
+print(10-8) # =2
 

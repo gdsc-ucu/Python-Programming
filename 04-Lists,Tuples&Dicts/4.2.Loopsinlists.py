3f211cecf9f96d29d2dfd04fbf3f554bf4dfa32e
Ashaba <ashabajasper@gmail.com>
2022-10-18 22:57:40 +0300

    This is an updated change

---
+++ 04-Lists,Tuples&Dicts/4.2.Loopsinlists.py
@@ -0,0 +1,8 @@
+fruits=["oranges","mangoes","apples","pawpaws","watermelon"]
+#prints all the items in the list
+for i in fruits:
+    print(i)
+print("*****************")
+
+for i in range(len(fruits)):
+    print (fruits[i])

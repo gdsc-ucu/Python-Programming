a6d675fd58d318d3c2971045deda1eb553c43fc3
Ashaba <ashabajasper@gmail.com>
2022-10-18 12:18:22 +0300

    Update-2

--- 01-Basics/1.2.FurtherExpressions.py
+++ 01-Basics/1.2.FurtherExpressions.py
@@ -14,5 +14,14 @@ print(13//4) #returns the rounded down division output =3
 #In order pf precedence:
 # **,*,/,//,% to overide this, parenthesis() can be used
 
+print(2+3*6) #=20
+print((2+3)*6) #=30
+
+#evaluate
+print((5-1)*((7+1)/(3-1))) #=16.0
+
+print(5-1*7+1/3-1) #=-2.666666666666667
+
+
 
 

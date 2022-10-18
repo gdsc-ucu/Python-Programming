8b092d5402ccafa6ed7608a93cc142c2bf2a844c
Ashaba <ashabajasper@gmail.com>
2022-10-18 16:14:56 +0300

    This is an updated change

--- 02-Flow Control/2.4.forloop.py
+++ 02-Flow Control/2.4.forloop.py
@@ -1,2 +1,13 @@
 #The mother of all loops, the For loop
-#
+#The while loop keeps loopimh while its condition is True
+#If you want to execute for a certain number of times, do this with a for loop and range()
+
+#prints each letter in your name in a row
+name=str(input("Please enter your name:"))
+for i in name:
+    print(i)
+
+#prints each letter in your name in a column
+name=str(input("Please enter your name:"))
+for i in name:
+    print(i,end="") #end parameter introduced

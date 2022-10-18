d49f11d80b27a339e6baa20b7a7f080882a2087a
Ashaba <ashabajasper@gmail.com>
2022-10-18 12:28:24 +0300

    Update-3

---
+++ 01-Basics/1.3.DataTypes&Variables.py
@@ -0,0 +1,11 @@
+#Data types are categories for values
+#Python has 3 main Data Types: Intergers,Floats,Strings
+#Intergers:Literally numbers
+print(-2,-2,0,3,4,5,234000,5600,4500)
+
+#Floats: Decimal values
+print(-1.25,-1.0,-2.456,0.5,1.25)
+
+#Strings: characters, text
+#either "" or '' can be used
+print("Hey", "a", 'a', '12 dogs', "13" )

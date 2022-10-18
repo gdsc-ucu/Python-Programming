f1abb82bd48ca4dbc72b89049d16f55019d8c52e
Ashaba <ashabajasper@gmail.com>
2022-10-18 22:28:57 +0300

    This is an updated change

--- 04-Lists,Tuples&Dicts/4.1.BasicList.py
+++ 04-Lists,Tuples&Dicts/4.1.BasicList.py
@@ -1,7 +1,7 @@
 #is a value that contains multiple values in an ordered sequence
 list1=[1,2,3]
 list2=['cat','bat','rat','elephant']
-list3=[12,45,65]
+list3=[12,45,65,78,98,607,43]
 print(list1)
 print(list2)
 print(list3)
@@ -9,3 +9,22 @@ print(list3)
 #access elements in a list by using indexes
 print(list1[0])
 print(list1[-1])
+
+#slicing
+print(list2[0:3]) #prints values from the first to the third (0,1,2)-indices
+#same as
+print(list2[:2])
+#same as
+print(list2[0:-1])
+
+#using the len() function
+print(len(list1))
+print(len(list2))
+print(len(list3))
+
+#changing values in lists
+#using list1 as an example list1=[1,2,3]
+list1[0]=4
+print(list1)#replaces the value at index 0-1 to 4
+
+#list concatenation

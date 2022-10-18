9b17df6523be526a64769d483cf6e9e2f2b4789d
Ashaba <ashabajasper@gmail.com>
2022-10-18 14:34:36 +0300

    This is an updated change

---
+++ 02-Flow Control/IfStatements.py
@@ -0,0 +1,7 @@
+#the most common type of flow control is the if statement
+#the condition happens and if True, excutes the code in the main body if not, exits of the if function eg
+print("Please enter your name(if you are not registered please do):")
+name=input()
+if name=="Ruth":
+    print("Hey Ruth, welcome")
+#authenticates Ruth, any other name, the process will exit

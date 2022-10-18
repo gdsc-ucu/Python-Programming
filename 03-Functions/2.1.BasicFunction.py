bed630dc458487428974586d1fdc5f9342a972c9
Ashaba <ashabajasper@gmail.com>
2022-10-18 21:18:40 +0300

    This is an updated change

--- 03-Functions/2.1.BasicFunction.py
+++ 03-Functions/2.1.BasicFunction.py
@@ -1,8 +1,10 @@
-<<<<<<< HEAD
+#function is a mini program
+#def statememt defines the function in this case names hello
 def hello():
+    #body of the function
     print("Hello There")
     print("My name is Jasper")
     print("Your trainer for this session")
-
+#calling the function
 hello()
 

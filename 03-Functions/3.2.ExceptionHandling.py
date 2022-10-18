e7655c70c3cd533ffe22dce499d31039b38e45e1
Ashaba <ashabajasper@gmail.com>
2022-10-18 21:46:03 +0300

    This is an updated change

---
+++ 03-Functions/3.2.ExceptionHandling.py
@@ -0,0 +1,9 @@
+#enabling the program to detect errors, handle them and continue to run
+#program to see how many numbers can divide 2022
+def division(divisor):
+    return 2022/divisor
+
+print(division(2))
+print(division(12))
+print(division(-29))
+print(divison(0))

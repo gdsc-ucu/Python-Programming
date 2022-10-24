7d97be2c4bd59c7c41d863f6b0d78d25f07fd6fc
Ashaba <ashabajasper@gmail.com>
2022-10-24 12:43:53 +0300

    Update

---
+++ 04-Lists,Tuples&Dicts/4.5.Tuples.py
@@ -0,0 +1,6 @@
+#immutable and mutable data types
+#tuples are similar to lists except: parenthesis used() and cannot be modified
+tuple=('howdy','mel','shaggy','scooby')
+print(tuple[1])
+print(tuple[0:2])
+print(len(tuple))

f690e7aa6514e537ab86975f740240277529dff6
Ashaba <ashabajasper@gmail.com>
2022-10-24 12:01:20 +0300

    This is an updated change

--- 04-Lists,Tuples&Dicts/4.2.Loopsinlists.py
+++ 04-Lists,Tuples&Dicts/4.2.Loopsinlists.py
@@ -13,4 +13,10 @@ print("#################")
 #prints with the index
 for i in range(len(fruits)):
     print(f"Index-{str(i)} in fruits is {fruits[i]}")
+print("#################")
+
+#you can also use the enumerate function with lists
+#using fruits as the original list
+for index,item in enumerate(fruits):
+    print(f"Index-{str(index)} in fruits is {item}")
 

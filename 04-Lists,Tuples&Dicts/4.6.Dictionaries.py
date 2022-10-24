db1399e717346f32db84ebc0035a792a67398267
Ashaba <ashabajasper@gmail.com>
2022-10-24 12:53:33 +0300

    Update

--- 04-Lists,Tuples&Dicts/4.6.Dictionaries.py
+++ 04-Lists,Tuples&Dicts/4.6.Dictionaries.py
@@ -4,3 +4,5 @@
 student={'name':"Dondox Krout", 'age':21, "Height":"121 cm", "Class":"First Year"}
 print(student)
 print("Name of student is:", student['name'])
+
+#more on dictionaries after successful completion of Data Structures and Algorithms in Python

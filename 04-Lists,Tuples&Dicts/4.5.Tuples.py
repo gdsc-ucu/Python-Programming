9c5cdae47885dcf2719d9c574e82a82da612d9ab
Ashaba <ashabajasper@gmail.com>
2022-10-24 12:47:22 +0300

    Update

--- 04-Lists,Tuples&Dicts/4.5.Tuples.py
+++ 04-Lists,Tuples&Dicts/4.5.Tuples.py
@@ -4,3 +4,10 @@ tuple=('howdy','mel','shaggy','scooby')
 print(tuple[1])
 print(tuple[0:2])
 print(len(tuple))
+
+#there isn;t much to talk about tuples though
+#use the type function to understand difference between a tuple and a string
+
+print(type("hello",))
+print(type("hello"))
+#to be revisted

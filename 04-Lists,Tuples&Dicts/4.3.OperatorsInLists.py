6470d11e295e385714e78cdd9dd9643fa4097811
Ashaba <ashabajasper@gmail.com>
2022-10-24 11:37:54 +0300

    This is an updated change

---
+++ 04-Lists,Tuples&Dicts/4.3.OperatorsInLists.py
@@ -0,0 +1,7 @@
+#you can use the in and not in operators in lists for verification
+my_list=['dog','cat','fish','monkey']
+
+print('lion' in my_list)
+#will putput a boolean statement verifying if it is there or not
+
+print('lion' not in my_list)

f0028a604a6af7f518c11b197114b597323c50e3
Ashaba <ashabajasper@gmail.com>
2022-10-24 12:53:04 +0300

    Update

---
+++ 04-Lists,Tuples&Dicts/4.6.Dictionaries.py
@@ -0,0 +1,6 @@
+#dictionary is a mutable collection of many values
+#associated with a key-value pair relationship
+
+student={'name':"Dondox Krout", 'age':21, "Height":"121 cm", "Class":"First Year"}
+print(student)
+print("Name of student is:", student['name'])

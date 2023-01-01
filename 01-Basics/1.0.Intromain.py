b2ce8e31a1c9db4e78a3f7570971a3938f1dda8e
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-01-01 19:12:37 +0300

    Update 1.0.Intromain.py

--- 01-Basics/1.0.Intromain.py
+++ 01-Basics/1.0.Intromain.py
@@ -8,6 +8,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+#updated 2023
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
